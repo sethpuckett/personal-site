545910771cedbc58e77879fff40a925c97d78d86
Seth Puckett <puckett.seth@gmail.com>
2016-10-18 16:17:42 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#slideshow').googleslides({
+        userid:'115528839112598673902', 
+        albumid:'5710317752556741025',
+        imgmax: 390,
+        albumlink: false,
+        time: 10000
+    });
+});
