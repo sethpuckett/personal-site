38a5286787b60383d63b10ca4c56b3e38531502e
Seth Puckett <puckett.seth@gmail.com>
2016-11-10 15:54:08 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+
+  var SPREADSHEET_KEY = '0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE';
+
+  Tabletop.init({ 
+    key: SPREADSHEET_KEY,
+    callback: loadSpreadsheetData
+  });
+
   $(".side-nav a").click(function(event) {
     event.preventDefault();
     var id = $(this).attr("href");
@@ -8,5 +16,22 @@ $(document).ready(function() {
 
     $(".nav-content .nav-pane").removeClass("active");
     $(id).addClass("active");
-  })
+  });
+
 });
+
+function loadSpreadsheetData(data, tabletop) {
+
+}
+
+function populateSchedule(scheduleData) {
+  $("#schedule-loader").hide();
+}
+
+function populateWriting(writingData) {
+  $("#writing-loader").hide();
+}
+
+function populateProjects(projectsData) {
+  $("#projects-loader").hide();
+}
